68eaeaf4b18b6346fa083f59fb3c52e47c83eb15
Andrey Kokoev <andrei.kokoev@gmail.com>
2024-10-09 18:53:25 -0500

    works in local

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,47 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as fs from 'fs';
+import * as path from 'path';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "ai-workflows-helper" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('ai-workflows-helper.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from ai-workflows-helper!');
+	let disposable = vscode.commands.registerCommand('ai-workflows-helper--vscode-extension.copyFiles', (uri, uris) => {
+	  copyFilesAsTextStream(uris);
 	});
-
+  
 	context.subscriptions.push(disposable);
-}
+  }
 
 // This method is called when your extension is deactivated
 export function deactivate() {}
+
+async function copyFilesAsTextStream(uris: vscode.Uri[]) {
+	if (!uris || uris.length === 0) {
+	  vscode.window.showWarningMessage('No files selected.');
+	  return;
+	}
+  
+	let textStream = '';
+  
+	// Process files in the order they were selected
+	for (const uri of uris) {
+	  const filePath = uri.fsPath;
+	  const fileName = path.basename(filePath);
+  
+	  try {
+		const fileContent = await vscode.workspace.fs.readFile(uri);
+		const contentString = Buffer.from(fileContent).toString('utf8');
+  
+		textStream += `--- ${fileName} ---\n${contentString}\n\n`;
+	  } catch (error) {
+		vscode.window.showErrorMessage(`Failed to read file: ${fileName}`);
+	  }
+	}
+  
+	// Copy to clipboard
+	await vscode.env.clipboard.writeText(textStream);
+  
+	vscode.window.showInformationMessage('Files copied as text stream to clipboard.');
+  }
